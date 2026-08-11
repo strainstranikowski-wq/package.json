7e5058a289d2bdece4e770fddf71efb19c043047
strainstranikowski-wq <strainstranikowski@gmail.com>
2026-08-11 03:21:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
   ]
 });
 
-client.once('ready', () => {
+client.once("ready", () => {
   console.log(`Bot zalogowany jako ${client.user.tag}`);
 });
 
